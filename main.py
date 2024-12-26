2cb21fbc51ba00f5b63e3d855a4fc93aeab297e7
Selwyn <jaymeselwyn@gmail.com>
2024-12-26 15:59:49 +0800

    .

--- main.py
+++ main.py
@@ -118,3 +118,6 @@ async def websocket_endpoint(websocket: WebSocket):
         await websocket.close()
 
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
